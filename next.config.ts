2f90ef74151e9ef45e94257fdeff203b9a94a615
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-05-21 14:11:38 -0300

    11: products

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    resolveAlias: {
+      underscore: 'lodash',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  }
 };
 
 export default withPayload(nextConfig);
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,13 @@
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import {
+    ProductList,
+    ProductListSkeleton,
+} from "@/modules/products/ui/components/products-list";
+
 interface Props {
     params: Promise<{
         category: string;
@@ -7,10 +17,17 @@ interface Props {
 const Page = async ({ params }: Props) => {
     const { category } = await params;
 
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category,
+    }));
+
     return (
-        <div>
-            Category: {category}
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <ProductList category={category} />
+            </Suspense>
+        </HydrationBoundary>
     );
 };
 
