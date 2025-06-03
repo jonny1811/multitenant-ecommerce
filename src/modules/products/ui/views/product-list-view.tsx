264744be64db793a6a014e57b8eef5e13dd1245c
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-06-03 16:15:34 -0300

    16: tenant pages

--- src/modules/products/ui/views/product-list-view.tsx
+++ src/modules/products/ui/views/product-list-view.tsx
@@ -6,10 +6,14 @@ import { ProductList, ProductListSkeleton } from "../components/products-list";
 
 interface Props {
     category?: string;
+    tenantSlug?: string;
+    narrowView?: boolean;
 };
 
 export const ProductListView = ({
     category,
+    tenantSlug,
+    narrowView,
 }: Props) => {
     return (
         <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
@@ -24,8 +28,8 @@ export const ProductListView = ({
                     <ProductFilters />
                 </div>
                 <div className="lg:col-span-4 xl:col-span-6">
-                    <Suspense fallback={<ProductListSkeleton />}>
-                        <ProductList category={category} />
+                    <Suspense fallback={<ProductListSkeleton narrowView={narrowView} />}>
+                        <ProductList category={category} tenantSlug={tenantSlug} narrowView={narrowView} />
                     </Suspense>
                 </div>
             </div>
